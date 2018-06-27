9264a681605d757ea0a267a15e586b962b13ce30
dexia2 <lindan.k2012.af.nk@gmail.com>
2018-06-27 12:32:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+to-amazon
+===============
+
+文字を選択してコンテキストメニューを開くことで、キーワードのAmazonの検索ページを別タブで開きます。
+似た拡張は他にもありますが、日本語ページに飛びたかったので作成しました。
+
+## 著者
+
++ dexia2
+    + Qiita
+        + https://qiita.com/dexia
+	+ twitter
+		+ https://twitter.com/dexia2
+	+ ブログ
+		+ http://howitworks.hatenablog.jp/
+
+## License
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
---
+++ assets/js/background.js
@@ -0,0 +1,15 @@
+(function () {
+
+    chrome.contextMenus.create({
+        title: "Amazonで検索",
+        contexts: ["selection"],
+        type: "normal",
+        onclick: function (info) {
+            var keyword = encodeURI(info.selectionText);
+            chrome.tabs.create({
+                url: "https://www.amazon.co.jp/s/?field-keywords=" + keyword
+            });
+        }
+    });
+
+}).call(this);
